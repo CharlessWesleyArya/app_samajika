362b87992e241995d667386ab45d62b1868e0521
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-06-28 01:15:52 +0530

    created some actions and ready to create reducer folder

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Auth.css'
 import Logo from '../../img/logo.png'
+import { useState } from 'react'
+import {useDispatch} from 'redux'
+import { logIn, signUp } from '../../Actions/AuthAction'
 const Auth = () => {
+  const [isSignUp,setIsSignUp]=useState(true);
+  const dispatch=useDispatch();
+  const [data,setData]=useState({firstName:'',lastName:'',userName:'',password:'',confirmPassword:""})
+
+  const handleChange=(e)=>{
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+
+  const [confirmPassword,setConfirmPassword]=useState(true)
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(isSignUp){
+      /* if(data.password!==data.confirmPassword){
+        setConfirmPassword(false)
+      } */
+      data.password===data.confirmPassword?dispatch(signUp(data)):setConfirmPassword(false)
+    }else{
+      dispatch(logIn(data))
+    }
+  }
+  const resetForm=()=>{
+    setConfirmPassword(true);
+    setData({
+      firstName:'',lastName:'',userName:'',password:'',confirmPassword:""
+    })
+  }
+
   return (
     <div className="Auth">
+      {/* left side */}
         <div className="a-left">
             <img src={Logo} alt="" />
             <div className="webName">
@@ -11,10 +42,32 @@ const Auth = () => {
                 <h6>Connect With Loved Once</h6>
             </div>
         </div>
-        <LogIn />
+        {/* Right-side */}
+        <div className='a-right'>
+      <form className='infoForm authForm' onSubmit={handleSubmit}>
+        <h3>{isSignUp? "Sign Up":"Login"}</h3>
+        {isSignUp &&
+        <div>
+          <input type="text"  placeholder='First Name' className='infoInput' name="firstName" onChange={handleChange} value={data.firstName}/>
+          <input type="text"  placeholder='Last Name' className='infoInput' name="lastName" onChange={handleChange} value={data.lastName}/>
+        </div>}
+        <div>
+          <input type="text" className='infoInput' name='userName' placeholder='User Name' onChange={handleChange} value={data.userName}/>
+        </div>
+        <div>
+          <input type="password" className="infoInput" name='password' placeholder='Password'onChange={handleChange} value={data.password}/>
+          {isSignUp&&<input type="password" className="infoInput" name='confirmPassword' placeholder='Confirm Password' onChange={handleChange} value={data.confirmPassword}/>}
+        </div>
+        <span style={{display:confirmPassword?"none":"block",color:"red",fontSize:'12px',alignSelf:'flex-end',marginRight:"5px"}}>* Confirm Password is not Same</span>
+        <div>
+          <span style={{fontSize:'12px',cursor:'pointer'}} onClick={()=>{setIsSignUp((prev)=>!prev);resetForm()}}>{isSignUp?"Already have an account. LOGIN!":"Don't have an account? Sign Up"}</span>
+        </div>
+        <button className='button infoButton' type='submit'>{isSignUp?"Sign Up":"Login"}</button>
+      </form>
+    </div>
     </div>
   )
-}
+}/* 
 function LogIn(){
   return(
     <div className="a-right">
@@ -58,5 +111,5 @@ function SignUp(){
     </div>
   )
 }
-
+ */
 export default Auth
